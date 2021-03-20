da17b5e12850086c5627f52c7bfd4149197e4bfb
Vishwas Rao <vhebbur@moonshot.cels.anl.gov>
2021-03-19 22:59:47 -0500

    Parallel Code

--- Parallel_Matern.jl
+++ Parallel_Matern.jl
@@ -1,9 +1,9 @@
-using NPZ
-include("MaternKernelApproximation.jl")
-z3d = npzread("Notebooks/volume_data_movo.npy");
-x = npzread("Notebooks/xaxis.npy");
-x2 = npzread("Notebooks/yaxis.npy");
-x3 = npzread("Notebooks/zaxis.npy");
+#using NPZ
+#include("MaternKernelApproximation.jl")
+#z3d = npzread("Notebooks/volume_data_movo.npy");
+#x = npzread("Notebooks/xaxis.npy");
+#x2 = npzread("Notebooks/yaxis.npy");
+#x3 = npzread("Notebooks/zaxis.npy");
 xmin = 1;
 xmax = 6;
 ymin = 1;
@@ -18,22 +18,31 @@ z3d_restored = copy(z3d);
 stride = 20;
 h = 0.02;
 # starttime = timeit.default_timer()
-Threads.@threads for i = zmin:zmax-1
+cartesian_product_boxes = []
+for i = zmin:zmax-1
     i1 = Int((i-zbegin) /h)-stride;
     i2 = i1+2*stride;
-    #print(i1,i2)
     for j = ymin:ymax-1
         j1 = Int((j-ybegin)/h)-stride;
         j2 = j1+2*stride;
-        #print(j1,j2)
         for k = xmin:xmax-1
             k1 = Int((k-ybegin)/h) - stride;
             k2 = k1+2*stride;
-            #print(k1,k2)
-            z3temp = z3d_copy[k1+1:k2,j1+1:j2,i1+1:i2];
-            restored_img, punched_image = Laplace3D_Grid(x[k1+1:k2], x2[j1+1:j2], x3[i1+1:i2], z3temp, epsilon, radius, h);
-            restored_img_reshape = reshape(restored_img, (2*stride,2*stride,2*stride));
-            z3d_restored[k1+1:k2, j1+1:j2, i1+1:i2] = restored_img_reshape;
+            append!(cartesian_product_boxes,[(i1,i2,j1,j2,k1,k2)]);
         end
     end
+end
+
+
+Threads.@threads for i = 1:length(cartesian_product_boxes)
+    i1 = cartesian_product_boxes[i][1]
+    i2 = cartesian_product_boxes[i][2]
+    j1 = cartesian_product_boxes[i][3]
+    j2 = cartesian_product_boxes[i][4]
+    k1 = cartesian_product_boxes[i][5]
+    k2 = cartesian_product_boxes[i][6]
+    z3temp = z3d_copy[k1+1:k2,j1+1:j2,i1+1:i2];
+    restored_img, punched_image = Laplace3D_Grid(x[k1+1:k2], x2[j1+1:j2], x3[i1+1:i2], z3temp, epsilon, radius, h);
+    restored_img_reshape = reshape(restored_img, (2*stride,2*stride,2*stride));
+    z3d_restored[k1+1:k2, j1+1:j2, i1+1:i2] = restored_img_reshape;
 end
