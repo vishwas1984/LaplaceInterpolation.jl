402b9632b4cd8d1e553ac1b1cf230fe438a351b8
clhaley <haley@anl.gov>
2021-05-19 09:55:33 -0500

    obselete

--- obsolete/test_Parallel_Mat.jl
+++ obsolete/test_Parallel_Mat.jl
@@ -1,5 +1,4 @@
 
-#=
 include("MaternKernelApproximation.jl")
 (xmin, xmax) = (ymin, ymax) = (zmin,zmax) = (0.0, 1.0)
 xpoints = ypoints = zpoints = -0.2:0.2:1.2
@@ -12,8 +11,6 @@ restored = Parallel_Matern3D_Grid(xpoints, ypoints, zpoints, imgg, epsilon, radi
                                   h, k, l, xmin, xmax, ymin, ymax, zmin, zmax, m)
 restored = Parallel_Laplace3D_Grid(xpoints, ypoints, zpoints, imgg, radius, 
                                   h, k, l, xmin, xmax, ymin, ymax, zmin, zmax)
-=#
 
-include("GeneralMK3D.jl")
 
 
