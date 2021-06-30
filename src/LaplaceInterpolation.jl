046b70af80aed09b7d24a5ef3c7a91187a232f09
Charlotte Haley <haleycharlotte@gmail.com>
2021-06-30 12:54:40 -0500

    add to master file

--- src/LaplaceInterpolation.jl
+++ src/LaplaceInterpolation.jl
@@ -3,7 +3,7 @@ module LaplaceInterpolation
   using LinearAlgebra, SparseArrays
 
   include("Matern1D2D.jl")
-  export ∇²1d_Grid, ∇²2d_Grid, Matern_1D_Interplation, Matern1D, Matern2D, Matern2D_Grid 
+  export ∇²1d_Grid, ∇²2d_Grid, Matern_1D_Grid,  Matern_1D_Interplation, Matern1D, Matern2D, Matern2D_Grid 
 
   include("GeneralMK3D.jl")
   export ∇²3d_Grid, Matern_3D_Grid, Laplace_3D_Grid, parallel_Matern_3DGrid
