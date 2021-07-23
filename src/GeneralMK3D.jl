5dc3dfcab9eca4371836128dd0d2b90095515356
Charlotte Haley <haleycharlotte@gmail.com>
2021-07-22 22:34:04 -0500

    char
    Former-commit-id: ca83d049905f38130631e55574478cdeddb1f967
    Former-commit-id: 295a0b5733379f44be015a83dfa1e3e9e1854d1c

--- src/GeneralMK3D.jl
+++ src/GeneralMK3D.jl
@@ -139,7 +139,7 @@ Interpolate, in parallel and in-place, multiple punches
 ...
 """
 function parallel_mat(imgg, Qh_min, Qh_max, Qk_min, Qk_max, Ql_min, Ql_max,
-                        m = 1, eps = 0.0, h = 1.0, k = 1.0, l = 1.0, symm = 'all')
+                        m = 1, eps = 0.0, h = 1.0, k = 1.0, l = 1.0, symm = 'G')
     centers = center_list(symm, Qh_min, Qh_max, Qk_min, Qk_max, Ql_min, Ql_max)
     discard = punch_3Dd_cart.(centers, radius, x, y, z)
     # Threads.@threads for d in discard
