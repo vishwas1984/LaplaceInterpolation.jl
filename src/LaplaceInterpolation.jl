8a7f8a528608b329e9e57006987f941158d57bbc
Charlotte Haley <haleycharlotte@gmail.com>
2021-07-22 22:27:46 -0500

    export center_list
    Former-commit-id: 27ba3ebdbeaa7136c78c78e56dc501d5e9242c0e
    Former-commit-id: 09f0b7bc0e1434bc76a36f296c908d05043afe71

--- src/LaplaceInterpolation.jl
+++ src/LaplaceInterpolation.jl
@@ -6,14 +6,14 @@ module LaplaceInterpolation
   export nablasq_grid, bdy_nodes, matern_1d_grid, matern_2d_grid 
 
   include("GeneralMK3D.jl")
-  export nablasq_3d_grid, matern_3d_grid, parallel_mat!
+  export nablasq_3d_grid, matern_3d_grid, parallel_mat
 
   include("MaternKernelApproximation.jl")
   export spdiagm_nonsquare, return_boundary_nodes
   export Matern3D_Grid, Parallel_Matern3D_Grid
 
   include("punch.jl")
-  export punch_holes_3D, punch_holes_2D, punch_3d_cart
+  export punch_holes_3D, punch_holes_2D, punch_3d_cart, center_list 
 
   include("nexus.jl")
   # These functions are unexported
